57082364799e8889c803b74a18f6ef82175286bd
innesbre <brendan.innes@mail.utoronto.ca>
2018-08-26 20:35:00 -0400

    For Brenda

---
+++ R/viewRetinaSC.R
@@ -0,0 +1,37 @@
+#' View HumanLiver data in the scClustViz Shiny app
+#'
+#' A wrapper function to view the \code{HumanLiver} dataset in the
+#' \code{scClustViz} Shiny app.
+#'
+#' @param outPath Default = "./" (the working directory). Specify the directory
+#'   used to save/load any analysis files you generate while exploring the
+#'   \code{HumanLiver} data.
+#'
+#' @return The function causes the scClustViz Shiny GUI app to open in a
+#'   seperate window.
+#'
+#' @examples
+#'   viewRetinaSC()
+#'
+#' @seealso \url{https://baderlab.github.io/scClustViz} for information on
+#'   \code{scClustViz}.
+#'
+#' @export
+
+viewRetinaSC <- function(outPath="./") {
+  filePath <- system.file("RSCs/MouseRetinaSCs.RData",package="RetinaSC")
+  cellMarkers <- list()
+
+  if (require("org.Mm.eg.db",quietly=T)) {
+    annotationDB <- org.Mm.eg.db
+    scClustViz::runShiny(filePath=filePath,
+                         outPath=outPath,
+                         cellMarkers=cellMarkers,
+                         annotationDB=annotationDB)
+
+  } else {
+    scClustViz::runShiny(filePath=filePath,
+                         outPath=outPath,
+                         cellMarkers=cellMarkers)
+  }
+}
---
+++ inst/RSCs/MouseRetinaSCs_intro.md
@@ -0,0 +1,1 @@
+MouseRetinaSCs: You can add to this preamble by editting D:/Dropbox/GDB/BrendaRetina/code_outputs/v3/MouseRetinaSCs_intro.md
